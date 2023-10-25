3e090613b6d00b3633e344e1266dbce7a3739712
Lukas Friman <lukas@dreamplay.net>
2023-10-25 08:40:16 +0200

    Adds hooks lapin consumer proc macro.

--- library_derive/src/lib.rs
+++ library_derive/src/lib.rs
@@ -5,38 +5,50 @@ use darling::FromMeta;
 use proc_macro2::{Span, TokenStream};
 use quote::{quote, quote_spanned};
 use syn::{
-    parse2, spanned::Spanned, Expr, ExprLit, ExprPath, FnArg, Ident, ItemTrait, Lit, LitStr,
-    TraitItem, TraitItemFn, Type,
+    parse2, spanned::Spanned, FnArg, Ident, ItemTrait, LitStr, TraitItem, TraitItemFn, Type,
 };
 
 #[derive(Debug, Default, Eq, PartialEq, FromMeta)]
-struct LapinHooksArgs {
-    pub no_value: Option<()>,
-    pub value_name: Option<syn::Ident>,
+struct HooksLapinProducerArgs {}
 
-    pub no_trait: Option<()>,
-    pub trait_name: Option<syn::Ident>,
-}
-
-#[derive(Debug, Default, Eq, PartialEq, FromMeta)]
-struct AmqpRouteArgs {
-    pub path: Option<syn::Ident>,
-}
-
-impl LapinHooksArgs {
+impl HooksLapinProducerArgs {
     pub fn parse(attr: TokenStream) -> Result<Self, TokenStream> {
         let attr_args = NestedMeta::parse_meta_list(attr).map_err(|e| quote_spanned! {
             e.span() =>
                 compile_error!(format!("Meta from attribute could not be parsed: {}", e).as_str());
         })?;
 
-        LapinHooksArgs::from_list(&attr_args).map_err(|e| {
+        HooksLapinProducerArgs::from_list(&attr_args).map_err(|e| {
             quote_spanned! {
                 e.span() =>
                 compile_error!("Args could not be parsed from meta: {}", e);
             }
         })
     }
+}
+
+#[derive(Debug, Default, Eq, PartialEq, FromMeta)]
+struct HooksLapinConsumerArgs {}
+
+impl HooksLapinConsumerArgs {
+    pub fn parse(attr: TokenStream) -> Result<Self, TokenStream> {
+        let attr_args = NestedMeta::parse_meta_list(attr).map_err(|e| quote_spanned! {
+            e.span() =>
+                compile_error!(format!("Meta from attribute could not be parsed: {}", e).as_str());
+        })?;
+
+        HooksLapinConsumerArgs::from_list(&attr_args).map_err(|e| {
+            quote_spanned! {
+                e.span() =>
+                compile_error!("Args could not be parsed from meta: {}", e);
+            }
+        })
+    }
+}
+
+#[derive(Debug, Default, Eq, PartialEq, FromMeta)]
+struct AmqpRouteArgs {
+    pub path: Option<syn::Ident>,
 }
 
 impl AmqpRouteArgs {
@@ -63,12 +75,106 @@ pub fn amqp_route(
     item
 }
 
+struct ParsedHooksTrait {
+    method_names: Vec<Ident>,
+    data_types: Vec<Type>,
+    route_declarations: Vec<Ident>,
+}
+
+fn parse_hooks_trait(
+    brace_token: syn::token::Brace,
+    items: Vec<TraitItem>,
+) -> Result<ParsedHooksTrait, TokenStream> {
+    let mut method_names: Vec<Ident> = Vec::new();
+    let mut data_types: Vec<Type> = Vec::new();
+    let mut route_declarations: Vec<Ident> = Vec::new();
+
+    for method in items {
+        let TraitItem::Fn(TraitItemFn { sig, attrs, .. }) = method else {
+            return Err(quote_spanned! {
+                brace_token.span =>
+                compile_error!("Items on trait must only be functions.", e);
+            });
+        };
+
+        let mut inputs = sig.inputs.into_iter();
+
+        let Some(FnArg::Receiver(self_arg)) = inputs.next() else {
+            return Err(quote_spanned! {
+                sig.paren_token.span =>
+                compile_error!("Functions must have self argument.");
+            });
+        };
+
+        if self_arg.mutability.is_none() {
+            return Err(quote_spanned! {
+                self_arg.self_token.span =>
+                compile_error!("Functions must have mutable self argument.");
+            });
+        }
+
+        let Some(FnArg::Typed(data_arg)) = inputs.next() else {
+            return Err(quote_spanned! {
+                self_arg.self_token.span =>
+                compile_error!("Functions must have data argument.");
+            });
+        };
+
+        let method_name = sig.ident;
+        let data_type = data_arg.ty.as_ref().clone();
+
+        const AMQP_ROUTE_ATTRIBUTE: &str = "amqp_route";
+        let Some(route_declaration) = attrs.into_iter().find(|attr| {
+            attr.meta
+                .path()
+                .get_ident()
+                .is_some_and(|path_ident| *path_ident == AMQP_ROUTE_ATTRIBUTE)
+        }) else {
+            return Err(quote_spanned! {
+                method_name.span() =>
+                compile_error!(r#"Every function must have an "amqp_route" attribute."#);
+            });
+        };
+
+        let route_declaration_span = route_declaration.meta.span();
+
+        let route_declaration = match route_declaration.meta {
+            syn::Meta::List(nv) => nv,
+            _ => {
+                return Err(quote_spanned! {
+                    route_declaration_span =>
+                    compile_error!(r#""amqp_route" attribute should be a list attribute - #[amqp_route(path = "...")]."#);
+                });
+            }
+        };
+
+        let route_declaration_meta = AmqpRouteArgs::parse(route_declaration.tokens)?;
+
+        let Some(route_declaration) = route_declaration_meta.path else {
+            return Err(quote_spanned! {
+                route_declaration_span =>
+                compile_error!(r#""amqp_route" attribute should have a path variable - #[amqp_route(path = "...")]."#);
+            });
+        };
+
+        method_names.push(method_name);
+        data_types.push(data_type);
+        route_declarations.push(route_declaration);
+    }
+
+    Ok(ParsedHooksTrait {
+        method_names,
+        data_types,
+        route_declarations,
+    })
+}
+
 #[proc_macro_attribute]
 pub fn hooks_lapin_producer(
     attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let _args = match LapinHooksArgs::parse(TokenStream::from(attr)) {
+    let _args = match HooksLapinProducerArgs::parse(TokenStream::from(attr)) {
         Ok(val) => val,
         Err(err) => return err.into(),
     };
@@ -83,93 +189,14 @@ pub fn hooks_lapin_producer(
     } = parse2(item.clone())
         .unwrap_or_else(|_| unimplemented!("Typestate can only be created using enums."));
 
-    let mut method_names: Vec<Ident> = Vec::new();
-    let mut data_types: Vec<Type> = Vec::new();
-    let mut route_declarations: Vec<Ident> = Vec::new();
-
-    #[allow(clippy::never_loop)]
-    for method in items {
-        let TraitItem::Fn(TraitItemFn { sig, attrs, .. }) = method else {
-            return quote_spanned! {
-                brace_token.span =>
-                compile_error!("Items on trait must only be functions.", e);
-            }
-            .into();
-        };
-
-        let mut inputs = sig.inputs.into_iter();
-
-        let Some(FnArg::Receiver(self_arg)) = inputs.next() else {
-            return quote_spanned! {
-                sig.paren_token.span =>
-                compile_error!("Functions must have self argument.");
-            }
-            .into();
-        };
-
-        if self_arg.mutability.is_none() {
-            return quote_spanned! {
-                self_arg.self_token.span =>
-                compile_error!("Functions must have mutable self argument.");
-            }
-            .into();
-        }
-
-        let Some(FnArg::Typed(data_arg)) = inputs.next() else {
-            return quote_spanned! {
-                self_arg.self_token.span =>
-                compile_error!("Functions must have data argument.");
-            }
-            .into();
-        };
-
-        let method_name = sig.ident;
-        let data_type = data_arg.ty.as_ref().clone();
-
-        const AMQP_ROUTE_ATTRIBUTE: &str = "amqp_route";
-        let Some(route_declaration) = attrs.into_iter().find(|attr| {
-            attr.meta
-                .path()
-                .get_ident()
-                .is_some_and(|path_ident| *path_ident == AMQP_ROUTE_ATTRIBUTE)
-        }) else {
-            return quote_spanned! {
-                method_name.span() =>
-                compile_error!(r#"Every function must have an "amqp_route" attribute."#);
-            }
-            .into();
-        };
-
-        let route_declaration_span = route_declaration.meta.span();
-
-        let route_declaration = match route_declaration.meta {
-            syn::Meta::List(nv) => nv,
-            _ => {
-                return quote_spanned! {
-                    route_declaration_span =>
-                    compile_error!(r#""amqp_route" attribute should be a list attribute - #[amqp_route(path = "...")]."#);
-                }
-                .into();
-            }
-        };
-
-        let route_declaration_meta = match AmqpRouteArgs::parse(route_declaration.tokens) {
-            Ok(val) => val,
-            Err(err) => return err.into(),
-        };
-
-        let Some(route_declaration) = route_declaration_meta.path else {
-            return quote_spanned! {
-                route_declaration_span =>
-                compile_error!(r#""amqp_route" attribute should have a path variable - #[amqp_route(path = "...")]."#);
-            }
-            .into();
-        };
-
-        method_names.push(method_name);
-        data_types.push(data_type);
-        route_declarations.push(route_declaration);
-    }
+    let ParsedHooksTrait {
+        method_names,
+        data_types,
+        route_declarations,
+    } = match parse_hooks_trait(brace_token, items) {
+        Ok(val) => val,
+        Err(err) => return err.into(),
+    };
 
     let impl_trait_ident = Ident::new(&trait_ident.to_string(), Span::call_site());
     let producer_name_string = format!("{}LapinProducer", trait_ident);
@@ -204,6 +231,89 @@ pub fn hooks_lapin_producer(
                     .await
                     .expect("TODO fix error handling")
             })*
+        }
+    }
+    .into()
+}
+
+#[proc_macro_attribute]
+pub fn hooks_lapin_consumer(
+    attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let _args = match HooksLapinConsumerArgs::parse(TokenStream::from(attr)) {
+        Ok(val) => val,
+        Err(err) => return err.into(),
+    };
+    let item = TokenStream::from(item);
+
+    let ItemTrait {
+        ident: trait_ident,
+        vis,
+        items,
+        brace_token,
+        ..
+    } = parse2(item.clone())
+        .unwrap_or_else(|_| unimplemented!("Typestate can only be created using enums."));
+
+    let ParsedHooksTrait {
+        method_names,
+        data_types,
+        route_declarations,
+    } = match parse_hooks_trait(brace_token, items) {
+        Ok(val) => val,
+        Err(err) => return err.into(),
+    };
+
+    let impl_trait_ident = Ident::new(&trait_ident.to_string(), Span::call_site());
+    let consumer_name_string = format!("{}LapinConsumer", trait_ident);
+    let consumer_name = Ident::new(&consumer_name_string, Span::call_site());
+    let consumer_name_str = LitStr::new(&consumer_name_string, Span::call_site());
+
+    quote! {
+        #item
+
+        #vis struct #consumer_name<'a, TConsumer: #impl_trait_ident + Sync + Send> {
+            consumer: &'a mut TConsumer,
+            #(#method_names: ::easymq::lapin::LapinConsumer<#data_types, ::serde_json::Error, fn(Vec<u8>) -> Result<#data_types, ::serde_json::Error>,>,)*
+        }
+
+        impl<'a, TConsumer: #impl_trait_ident + Sync + Send> ::std::fmt::Debug for #consumer_name<'a, TConsumer> {
+            fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+                ::std::fmt::DebugStruct::finish(&mut ::std::fmt::Formatter::debug_struct(f, #consumer_name_str))
+            }
+        }
+
+        impl<'a, TConsumer: #impl_trait_ident + Sync + Send> #consumer_name<'a, TConsumer> {
+            async fn new(
+                consumer: &'a mut TConsumer,
+                channel: &'a ::lapin::Channel,
+                consumer_tag: &str,
+            ) -> Result<#consumer_name<'a, TConsumer>, ::lapin::Error> {
+                Ok(Self {
+                    consumer,
+                    #(#method_names: ::easymq::lapin::LapinProducer::new(channel, #route_declarations, consumer_tag).await?,)*
+                })
+            }
+
+            async fn run(&mut self) -> Result<(), AmqpConsumerError<::lapin::Error, ::serde_json::Error>> {
+                #(let mut #method_names = ::easymq::Consumer::to_stream(&mut self.#method_names);)*
+        
+                select! {
+                #(
+                    result = #method_names.next() => {
+                        let Some(result) = result else {
+                            return Ok(());
+                        };
+                        let data = result?;
+    
+                        TConsumer::#method_names(self.consumer, data).await;
+                    }
+                )*
+                }
+        
+                Ok(())
+            }
         }
     }
     .into()
